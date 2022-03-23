35848a9fe5766c174b259375dce6741ff0185cd2
horningmp <matthew.horning@slu.edu>
2022-03-23 15:32:46 -0500

    added ignore for long line to db_wrapper

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,3 +1,6 @@
+# disabling:
+# C0301: Line too long (line-too-long)
+# pylint: disable=C0301
 '''Module for database'''
 import sqlite3
 from csvreader import read_file, write_file
@@ -14,4 +17,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
