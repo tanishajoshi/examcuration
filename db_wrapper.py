9ac8d7426e5b56998b6da8cc34467360102f3f17
horningmp <matthew.horning@slu.edu>
2022-03-23 15:31:18 -0500

    attempt number two

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,18 +1,17 @@
-import csv
+'''Module for database'''
+import sqlite3
 from csvreader import read_file, write_file
 
-import sqlite3
-"""
 connection = sqlite3.connect("qadb.db")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE question (qID INTEGER, question TEXT, qTypeID INTEGER, createDate, updateDate)")
 
-"""
 
 def main():
+    '''Main function'''
     write_file()
     read_file()
-    
 
 if __name__ == "__main__":
     main()
+    
