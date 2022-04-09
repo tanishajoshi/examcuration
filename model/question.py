c136482832ab2e34723065218d279ed2071feb41
horningmp <matthew.horning@slu.edu>
2022-04-09 17:01:34 -0500

    added tests for question.py

--- model/question.py
+++ model/question.py
@@ -5,17 +5,19 @@
 class QConstants:
     '''QConstants Class'''
     MYDB = "qadb"
-    #FREE_RESPONSE = 1
+    FREE_RESPONSE = 1
     #MULTIPLE_CHOICE = 2
-    BOOLEAN_CHOICE = 3 #Default question type for now
+    #BOOLEAN_CHOICE = 3 #Default question type for now
 
 class Question:
     '''Question Class'''
-    def __init__(self, question, question_type_id):
+    def __init__(self, question, question_type_id, question_answer=0):
         '''Initialization of question class.'''
         self.question_id = None
         self.question = question
         self.question_type_id = question_type_id
+        #TODO: Ensure question answer format is appropriate for question type
+        self.question_answer = question_answer # Default to NONE if no right answer or none provided
 
     def get_question_id(self):
         '''Return question id'''
@@ -32,3 +34,9 @@ class Question:
     def set_question_id(self, question_id):
         '''Set question id'''
         self.question_id = question_id
+
+    def get_answer(self):
+        return self.question_answer
+    
+    def update_answer(self, new_answer):
+        self.question_answer = new_answer
---
+++ model/test_question.py
@@ -0,0 +1,28 @@
+'''Module for testing question.py. Must be in same directory as question.py to avoid import errors.'''
+from model.question import Question, QConstants
+
+def test_generateQuestion():
+    '''Tests generating a question'''
+    sample_q = Question("What is my name?", 2, "Johhny Bravo")
+
+    assert (isinstance(sample_q, Question)) == True
+
+def test_updateAnswer():
+    '''Tests updating an answer to an already generated question'''
+    sample_q = Question("What is my name?", QConstants.FREE_RESPONSE, "Johhny Bravo")
+    sample_q.update_answer("Courage the Cowardly Dog")
+
+    assert sample_q.get_answer() == "Courage the Cowardly Dog"
+
+def test_set_question_id():
+    '''Tests setting a question ID'''
+    sample_q = Question("What is my name?", QConstants.FREE_RESPONSE, "Johhny Bravo")
+    sample_q.set_question_id(24)
+    assert sample_q.get_question_id() == 24
+    sample_q.set_question_id(sample_q.get_question_id() + 24)
+    assert sample_q.get_question_id() == 48
+
+def test_question_type():
+    '''Tests initialization of question properly sets Question Type'''
+    sample_q = Question("What is my name?", QConstants.FREE_RESPONSE, "Johhny Bravo")
+    assert sample_q.get_question_type_id() == QConstants.FREE_RESPONSE
