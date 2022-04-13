c8ea99019f03f678f3207eaba4a7480d0a7ee270
Tyler McMahon <tyler.mcmahon@slu.edu>
2022-04-12 19:40:52 -0500

    Fixed naming and unused import errors

--- qadriver.py
+++ qadriver.py
@@ -1,8 +1,5 @@
 """This is the main."""
-from view.qaview import handle_view
-from controller.qacontroller import setup
 from view.basicgui import start
 
 if __name__ == "__main__":
     start()
-    
