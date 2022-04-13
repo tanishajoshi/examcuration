cc6b2e0f9b459ea159424f9f19da08d3c72de7a3
Tyler McMahon <tyler.mcmahon@slu.edu>
2022-04-12 19:16:16 -0500

    Updated driver using gui

--- qadriver.py
+++ qadriver.py
@@ -1,7 +1,8 @@
 """This is the main."""
 from view.qaview import handle_view
 from controller.qacontroller import setup
+from view.basicgui import start
 
 if __name__ == "__main__":
-    setup()
-    handle_view()
+    start()
+    
